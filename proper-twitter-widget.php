bac3b74213d852a5f1b6c7b99b39f2a140608de8
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-05-09 00:15:21 +0100

    option to have transparent background

--- proper-twitter-widget.php
+++ proper-twitter-widget.php
@@ -4,11 +4,17 @@ Plugin Name: Proper Twitter Widget
 Plugin URI: https://github.com/mihai-rotaru/proper-twitter-widget
 Description: Displays a Twitter widget
 Author: Mihai Rotaru
-Version: 1.0.0
+Version: 1.1.0
 Author URI: https://github.com/mihai-rotaru
 */
  
 class ProperTwitterWidget extends WP_Widget {
+	/**
+	 * Default settings for Twitter widtgets
+	 */
+    protected static $twitter_defaults = array(
+        'transparent_bg' => false,
+    );
 
 	/**
 	 * Register widget with WordPress.
@@ -31,12 +37,17 @@ class ProperTwitterWidget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		extract( $args );
-		$twitter_id         = apply_filters( 'widget_twitter_id',       $instance['twitter_id'] );
-		$twitter_widget_id  = apply_filters( 'widget_twitter_widget_id',$instance['twitter_widget_id'] );
+		$twitter_id         = $instance['twitter_id'];
+		$twitter_widget_id  = $instance['twitter_widget_id'];
+        $transparent_bg     = !empty( $instance['transparent_bg'] ) ? '1' : '0';
 
 		echo $before_widget;
 		if ( ! empty( $twitter_id ) && ! empty( $twitter_widget_id ) ) {
-            $_out  = '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/' . $twitter_id ;
+            $_out  = '<a class="twitter-timeline" ';
+            if( $transparent_bg ) {
+                $_out .= 'data-chrome="transparent"';
+            }
+            $_out .= 'data-dnt="true" href="https://twitter.com/' . $twitter_id ;
             $_out .= '"  data-widget-id="' . $twitter_widget_id . '">Tweets by @' . $twitter_id . '</a>';
             $_out .= '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?';
             $_out .= '\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
@@ -59,6 +70,7 @@ class ProperTwitterWidget extends WP_Widget {
 		$instance = array();
 		$instance['twitter_id']         = strip_tags( $new_instance['twitter_id'] );
 		$instance['twitter_widget_id']  = strip_tags( $new_instance['twitter_widget_id'] );
+        $instance['transparent_bg']     = $new_instance['transparent_bg'] ? 1 : 0;
 
 		return $instance;
 	}
@@ -71,25 +83,23 @@ class ProperTwitterWidget extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		if ( isset( $instance[ 'twitter_id' ] ) ) {
-			$twitter_id = $instance[ 'twitter_id' ];
-		} else {
-			$twitter_id = __( '', 'text_domain' );
-		}
-		if ( isset( $instance[ 'twitter_widget_id' ] ) ) {
-			$twitter_widget_id = $instance[ 'twitter_widget_id' ];
-		} else {
-			$twitter_widget_id = __( '', 'text_domain' );
-		}
+        $instance = wp_parse_args( (array) $instance, $twitter_defaults );
+        $twitter_id = isset($instance['twitter_id']) ? esc_attr($instance['twitter_id']) : '';
+        $twitter_widget_id = isset($instance['twitter_widget_id']) ? esc_attr($instance['twitter_widget_id']) : '';
+        $transparent_bg = isset($instance['transparent_bg']) ? (bool) $instance['transparent_bg'] :false;
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id( 'twitter_id' ); ?>"><?php _e( 'Twitter username:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'twitter_id' ); ?>" name="<?php echo $this->get_field_name( 'twitter_id' ); ?>" type="text" value="<?php echo esc_attr( $twitter_id ); ?>" />
+            <label for="<?php echo $this->get_field_id( 'twitter_id' ); ?>"><?php _e( 'Twitter username:' ); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id( 'twitter_id' ); ?>" name="<?php echo $this->get_field_name( 'twitter_id' ); ?>" type="text" value="<?php echo esc_attr( $twitter_id ); ?>" />
 		</p>
 		<p>
-		<label for="<?php echo $this->get_field_id( 'twitter_widget_id' ); ?>"><?php _e( 'Widget ID:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'twitter_widget_id' ); ?>" name="<?php echo $this->get_field_name( 'twitter_widget_id' ); ?>" type="text" value="<?php echo esc_attr( $twitter_widget_id ); ?>" />
+            <label for="<?php echo $this->get_field_id( 'twitter_widget_id' ); ?>"><?php _e( 'Widget ID:' ); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id( 'twitter_widget_id' ); ?>" name="<?php echo $this->get_field_name( 'twitter_widget_id' ); ?>" type="text" value="<?php echo esc_attr( $twitter_widget_id ); ?>" />
 		</p>
+        <p>
+            <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('transparent_bg'); ?>" name="<?php echo $this->get_field_name('transparent_bg'); ?>"<?php checked( $transparent_bg ); ?> />
+            <label for="<?php echo $this->get_field_id('transparent_bg'); ?>"><?php _e( 'Transparent background ' ); ?></label><br />
+        </p>
 		<?php 
 	}
 
